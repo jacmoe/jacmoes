68864869a89960957ba6b22ecbbc719b3f2458b5
Jacob Moen <jacmoe.dk@gmail.com>
2016-11-27 05:42:48 +0100

    Uberspace logo added - Dreamhost logo removed

--- themes/bourbon/views/page/partials/_viewtop.php
+++ themes/bourbon/views/page/partials/_viewtop.php
@@ -2,4 +2,5 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 use jacmoe\mdpages\helpers\Page;
-$view->title = isset($page->title) ? $page->title : 'Untitled';
+$view->title = isset($page->title) ? $page->title: 'Untitled';
+$view->title = $view->title . " | Jacmoes Cyber Soapbox";
