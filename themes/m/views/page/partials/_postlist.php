d61479ecabd78d1203cebbea7d1518dc4d4b94d0
Jacob Moen <jacmoe.dk@gmail.com>
2017-11-01 19:36:29 +0100

    Schnack test

--- themes/m/views/page/partials/_postlist.php
+++ themes/m/views/page/partials/_postlist.php
@@ -1,5 +1,5 @@
 <?php
-if(Url::base(true) === 'https://local.jacmoe.dk') {
+if(Url::base(true) === 'https://jacmoe.dev') {
   $posts = Page::pages('datetime DESC', array('blogpost', '==', true));
 } else {
   $posts = Page::pages('datetime DESC', array('published', '==', true));
